62339c90b5cd2d77408daa010f1eb68fcce3c651
phuonganh <hnphuonganh279@gmail.com>
2025-12-17 04:21:58 -0600

    update

--- src/pages/DetailPage.ts
+++ src/pages/DetailPage.ts
@@ -1,44 +1,89 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 import { CommonPage } from "@src/common/CommonPage";
+import { BookingPage } from "./BookingPage";
 
 export class DetailPage extends CommonPage {
     private readonly gridParent = this.page.locator("//div[contains(@class,'MuiGrid-container') and contains(@class,'jss24')]");
-    private readonly imgEle = this.page.locator('.MuiGrid-root.jss14')
     private readonly gridListCinesEle = this.page.locator('#cinemaList')
 
-    private readonly lblDate = this.gridParent.locator(".//h4[contains(@class,'MuiTypography-h4')]");
-    private readonly lblTitle = this.gridParent.locator(".//h1[contains(@class,'MuiTypography-h1')]");
-    private readonly lblDuration = this.gridParent.locator(".//h5[contains(@class,'MuiTypography-h5')]");
-    private readonly btnBuy = this.gridParent.locator(".//a[normalize-space()='Mua vé']");
+    private readonly lblDate = this.gridParent.locator("//h4[contains(@class,'MuiTypography-h4')]");
+    private readonly lblTitle = this.page.locator("//h1[contains(@class,'MuiTypography-h1')]");
+    private readonly lblDuration = this.page.locator("//h5[contains(@class,'MuiTypography-h5')]");
+    private readonly btnBuyTicket = this.page.locator("//a[normalize-space()='Mua vé']");
 
     private readonly imgOuterCircle = this.page.locator('circle').first()
-    private readonly lblScore = this.gridParent.locator("div.MuiTypography-caption");
+    private readonly lblScore = this.page.locator("div.MuiTypography-caption");
     private readonly icoRating = this.gridParent.locator("//span[contains(@class,'MuiRating-root')]");
     private readonly icoStarFilled = this.icoRating.locator(".MuiRating-iconFilled");
     private readonly icoStars = this.icoRating.locator('.MuiRating-icon')
 
-    private readonly lblShowtimes = this.gridListCinesEle.locator(".//a[contains(@class,'jss40')]");
-
+    private readonly lblShowtimes = this.page.locator("//a[contains(@class,'jss40')]");
+    private readonly banner = this.page.locator('.jss19')
+    private readonly btnPlay = this.page.locator('button.MuiFab-root')
+    private readonly icoPlay = this.page.locator('img[alt="video-button"]')
+    private readonly modalVideo = this.page.locator('[role="dialog"]')
 
     constructor(page: Page) {
         super(page);
     }
 
-    getLblDate() {
-        return this.lblDate
+    getBanner(): Locator {
+        return this.banner
     }
 
-    getLblTitle() {
-        return this.lblTitle
+    getBtnPlay() {
+        return this.btnPlay
     }
 
-    getLblDuration() {
-        return this.lblDuration
+    getIcoPlay() {
+        return this.icoPlay
     }
 
-    getImgEle() {
-        return this.imgEle
+    getModalTrailer() {
+        return this.modalVideo
     }
 
+    async getFirstShowtimes() {
+        return this.getText(this.lblShowtimes.nth(0))
+    }
+
+    async pickFirstShowtimes(): Promise<BookingPage> {
+        await this.lblShowtimes.nth(0).click()
+
+        return new BookingPage(this.page)
+    }
+
+    async clickPlay() {
+        await this.btnPlay.click()
+    }
+
+    async clickBuy() {
+        await this.btnBuyTicket.click()
+    }
+
+    async getTotalStars() {
+        const stars = await this.icoStars.count()
+        return stars
+    }
     
+    async getFilledStars() {
+        const stars = await this.icoStarFilled.count()
+        return stars
+    }
+
+    async getScore() {
+        return await this.getText(this.lblScore)
+    }
+
+    async getTitle() {
+        return await this.getText(this.lblTitle)
+    }
+
+    async getDate() {
+        return await this.getText(this.lblDate)
+    }
+
+    async getDuration() {
+        return await this.getText(this.lblDuration)
+    }
 }
--- tests/movie-details/movie-details.spec.ts
+++ tests/movie-details/movie-details.spec.ts
@@ -2,14 +2,54 @@ import { test, expect } from '../../src/fixtures/custom-fixtures';
 test.describe("Movie Details", () => {
     test("TC0121: Verify movie info in detail page", async ({ detailPage }) => {
         const filmId = '9390'
-        const expectTitle = ''
-        const expectDate = ''
-        const expectShowtimes = ''
-        const expectDuration = ''
-        const expectScore = ''
-        const expectFilledStars = ''
-        const expectStars = ''
+        const expectTitle = 'AVATAR 2'
+        const expectDate = '16.12.2021'
+        const expectDuration = '120phút'
+        const expectScore = '10'
+        const expectFilledStars = 5
+        const expectStars = 5
+        const expectImg = 'url(\"http://movie0706.cybersoft.edu.vn/hinhanh/avatar-2_gp09.jpg\"), url(\"https://tix.vn/app/assets/img/default-film.webp\")'
         await detailPage.navigateTo(`https://demo1.cybersoft.edu.vn/detail/${filmId}`);
+
+        const banner = detailPage.getBanner()
+        await expect(banner).toBeVisible()
+        await expect(banner).toHaveCSS('background-image', expectImg)
+
+        expect(await detailPage.getTotalStars()).toStrictEqual(expectStars)
+        expect(await detailPage.getFilledStars()).toStrictEqual(expectFilledStars)
+        expect(await detailPage.getScore()).toStrictEqual(expectScore)
+        expect(await detailPage.getTitle()).toStrictEqual(expectTitle)
+        expect(await detailPage.getDate()).toStrictEqual(expectDate)
+        expect(await detailPage.getDuration()).toStrictEqual(expectDuration)
+
+        expect(detailPage.getIcoPlay()).not.toBeVisible()
+        await banner.hover()
+        expect(detailPage.getIcoPlay()).toBeVisible()
+
+        expect(detailPage.getBtnPlay()).toBeVisible()
+        expect(detailPage.getBtnPlay()).toBeEnabled()
+
+    })
+
+    test("TC0148: Verify interact with trailer", async ({ detailPage }) => {
+        const filmId = '9390'
+        await detailPage.navigateTo(`https://demo1.cybersoft.edu.vn/detail/${filmId}`);
+        await detailPage.clickPlay()
+
+        expect(detailPage.getModalTrailer()).toBeVisible()
+
+    })
+
+    test("TC0126: Verify the ‘Buy Ticket’ button when cinema information is available", async ({ detailPage }) => {
+        const expectShowtimes = "17-10-2021 ~ 08:43"
+        const filmId = '9390'
+        await detailPage.navigateTo(`https://demo1.cybersoft.edu.vn/detail/${filmId}`);
+        
+        await detailPage.clickBuy()
+        expect(await detailPage.getFirstShowtimes()).toMatch(/17-10-2021\s*~\s*08:43/)
+
+        const bookingPage = await detailPage.pickFirstShowtimes()
+        expect(bookingPage.page).toHaveURL(`https://demo1.cybersoft.edu.vn/purchase/45056`)
 
     })
 })
